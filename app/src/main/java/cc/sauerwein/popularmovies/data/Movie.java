88475130666256765444dd4a8d8c8b0f3096b49f
Rene Sauerwein <rene@sauerwein.cc>
2020-04-10 13:48:46 +0200

    Turn Movie into a room entity

--- app/src/main/java/cc/sauerwein/popularmovies/data/Movie.java
+++ app/src/main/java/cc/sauerwein/popularmovies/data/Movie.java
@@ -1,5 +1,9 @@
 package cc.sauerwein.popularmovies.data;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.util.Date;
@@ -9,16 +13,30 @@ import java.util.Date;
  * Stores all relevant movie metadata
  * As we create Movie objects with gson, we don't need a constructor
  */
+@Entity(tableName = "movies")
 public class Movie {
+    @PrimaryKey
     private int id;
     private String title;
     private String overview;
     @SerializedName("release_date")
+    @ColumnInfo(name = "release_date")
     private Date releaseDate;
     @SerializedName("poster_path")
+    @ColumnInfo(name = "poster_path")
     private String posterPath;
     @SerializedName("vote_average")
+    @ColumnInfo(name = "vote_average")
     private String userRating;
+
+    public Movie(int id, String title, String overview, Date releaseDate, String posterPath, String userRating) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.posterPath = posterPath;
+        this.userRating = userRating;
+    }
 
     public int getId() {
         return id;
