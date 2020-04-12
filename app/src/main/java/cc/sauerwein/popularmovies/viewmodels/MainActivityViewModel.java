e777a493bd2947fa3b31c381b55d0e8c3c908d2a
Rene Sauerwein <rene@sauerwein.cc>
2020-04-12 12:44:32 +0200

    Add getPopularMovies and getTopRatedMovies

--- app/src/main/java/cc/sauerwein/popularmovies/viewmodels/MainActivityViewModel.java
+++ app/src/main/java/cc/sauerwein/popularmovies/viewmodels/MainActivityViewModel.java
@@ -4,9 +4,26 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import cc.sauerwein.popularmovies.data.MovieList;
+import cc.sauerwein.popularmovies.data.Repository;
 
 public class MainActivityViewModel extends AndroidViewModel {
+
+    private Repository repository;
+
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
+
+        repository = Repository.getInstance(application);
+    }
+
+    public MutableLiveData<MovieList> getPopularMovies() {
+        return repository.getPopularMovies();
+    }
+
+    public MutableLiveData<MovieList> getTopRatedMovies() {
+        return repository.getTopRatedMovies();
     }
 }
