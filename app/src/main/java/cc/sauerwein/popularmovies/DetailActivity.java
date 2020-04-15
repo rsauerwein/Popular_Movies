97be29919333bc274483d14cf540e5ae2c4a2c5f
Rene Sauerwein <rene@sauerwein.cc>
2020-04-15 21:17:49 +0200

    Bind DetailActivity to view

--- app/src/main/java/cc/sauerwein/popularmovies/DetailActivity.java
+++ app/src/main/java/cc/sauerwein/popularmovies/DetailActivity.java
@@ -5,8 +5,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -26,11 +24,6 @@ public class DetailActivity extends AppCompatActivity {
 
     private Movie mMovie;
 
-    private TextView mMovieTitleTv;
-    private TextView mMovieDescriptionTv;
-    private TextView mUserRating;
-    private TextView mReleaseDate;
-    private ImageView mMovieThumbnail;
     private static final String LOG_TAG = DetailActivity.class.getSimpleName();
     private MovieDetailViewModel mViewModel;
     private ActivityDetailBinding mActivityBinding;
@@ -49,11 +42,6 @@ public class DetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String json = intent.getStringExtra(Intent.EXTRA_TEXT);
         mMovie = new Gson().fromJson(json, Movie.class);
-
-        mMovieTitleTv = findViewById(R.id.tv_movie_title);
-        mMovieDescriptionTv = findViewById(R.id.tv_description);
-        mUserRating = findViewById(R.id.tv_user_rating);
-        mReleaseDate = findViewById(R.id.tv_release_date);
 
         populateUI();
     }
