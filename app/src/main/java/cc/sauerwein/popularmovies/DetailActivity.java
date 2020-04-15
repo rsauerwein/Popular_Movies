0719ace62c8861ecb7002f181a22bfe0d1a423fc
Rene Sauerwein <rene@sauerwein.cc>
2020-04-15 21:17:21 +0200

    Bind DetailActivity to view

--- app/src/main/java/cc/sauerwein/popularmovies/DetailActivity.java
+++ app/src/main/java/cc/sauerwein/popularmovies/DetailActivity.java
@@ -1,6 +1,7 @@
 package cc.sauerwein.popularmovies;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,13 +9,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 import java.util.Calendar;
 
 import cc.sauerwein.popularmovies.data.Movie;
+import cc.sauerwein.popularmovies.databinding.ActivityDetailBinding;
+import cc.sauerwein.popularmovies.utilities.NetworkUtils;
 import cc.sauerwein.popularmovies.viewmodels.MovieDetailViewModel;
 
 public class DetailActivity extends AppCompatActivity {
@@ -27,6 +32,8 @@ public class DetailActivity extends AppCompatActivity {
     private TextView mReleaseDate;
     private ImageView mMovieThumbnail;
     private static final String LOG_TAG = DetailActivity.class.getSimpleName();
+    private MovieDetailViewModel mViewModel;
+    private ActivityDetailBinding mActivityBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,10 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         setTitle(getString(R.string.movie_detail));
+
+        mViewModel = new ViewModelProvider(this).get(MovieDetailViewModel.class);
+        mActivityBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
+        mActivityBinding.setViewModel(mViewModel);
 
         Intent intent = getIntent();
         String json = intent.getStringExtra(Intent.EXTRA_TEXT);
@@ -44,32 +55,25 @@ public class DetailActivity extends AppCompatActivity {
         mUserRating = findViewById(R.id.tv_user_rating);
         mReleaseDate = findViewById(R.id.tv_release_date);
 
-        setupViewModel();
         populateUI();
-    }
-
-    private void setupViewModel() {
-        MovieDetailViewModel viewModel = new ViewModelProvider(this).get(MovieDetailViewModel.class);
-        viewModel.getMovie(mMovie);
     }
 
     /**
      * Populate the Movie detail information to the UI
      */
     private void populateUI() {
-        mMovieTitleTv.setText(mMovie.getTitle());
-        mMovieDescriptionTv.setText(mMovie.getOverview());
-        mUserRating.setText(getString(R.string.movie_rating, mMovie.getUserRating()));
-        mMovieThumbnail = findViewById(R.id.iv_movie_poster_thumbnail);
+        mActivityBinding.tvMovieTitle.setText(mMovie.getTitle());
+        mActivityBinding.tvDescription.setText(mMovie.getOverview());
+        mActivityBinding.tvUserRating.setText(getString(R.string.movie_rating, mMovie.getUserRating()));
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(mMovie.getReleaseDate());
-        mReleaseDate.setText(String.format("%1d", calendar.get(Calendar.YEAR)));
+        mActivityBinding.tvReleaseDate.setText(String.format("%1d", calendar.get(Calendar.YEAR)));
 
         String posterPath = mMovie.getPosterPath();
-        //Uri posterUri = NetworkUtils.createPosterUri(posterPath);
-        //Picasso.get().load(posterUri).into(mMovieThumbnail);
-        mMovieThumbnail.setContentDescription(mMovie.getTitle());
+        Uri posterUri = NetworkUtils.createPosterUri(posterPath);
+        Picasso.get().load(posterUri).into(mActivityBinding.ivMoviePosterThumbnail);
+        mActivityBinding.ivMoviePosterThumbnail.setContentDescription(mMovie.getTitle());
     }
 
     public void favorite(View view) {
