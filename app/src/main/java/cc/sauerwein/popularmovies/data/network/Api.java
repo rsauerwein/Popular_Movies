0562db3c43b378a2f58c6b764c16f23401933664
Rene Sauerwein <rene@sauerwein.cc>
2020-04-18 12:56:30 +0200

    Code cleanup

--- app/src/main/java/cc/sauerwein/popularmovies/data/network/Api.java
+++ app/src/main/java/cc/sauerwein/popularmovies/data/network/Api.java
@@ -8,20 +8,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-// Todo cleanup member variables
 public class Api {
-
-    public static final String OPTION_POPULAR_MOVIES = "movie/popular";
-    public static final String OPTION_TOP_RATED_MOVIES_MOVIES = "movie/top_rated";
-    private static final String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
-    private static final String API_KEY_PARAM = "api_key";
-
-
-    private static final String DEFAULT_IMAGE_SIZE = "w185";
     private static final String API_BASE_URL = "https://api.themoviedb.org/3/";
     private static final String API_KEY = ApiKey.API_KEY; //Insert your API key here
     private static ApiInterface api;
-    private ApiInterface mRetrofitService;
 
     public static ApiInterface getApi() {
         if (api == null) {
