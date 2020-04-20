055e258c09a0e984723e4409eb2137a0e5531418
Rene Sauerwein <rene@sauerwein.cc>
2020-04-20 19:49:10 +0200

    Add getMovieDetails

--- app/src/main/java/cc/sauerwein/popularmovies/data/Repository.java
+++ app/src/main/java/cc/sauerwein/popularmovies/data/Repository.java
@@ -4,7 +4,10 @@ import android.app.Application;
 import android.util.Log;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
+import java.io.IOException;
 import java.util.List;
 
 import cc.sauerwein.popularmovies.data.db.AppDatabase;
@@ -73,5 +76,38 @@ public class Repository {
 
     public LiveData<List<Video>> fetchVideos(String movieId) {
         return Api.fetchVideos(movieId);
+    }
+
+    // Fetch movie details from Room and also from the API
+    // Possible issue: The code relies on that the Room request is faster than the API..
+    public MutableLiveData<Movie> getMovieDetails(Movie movie) {
+        final MutableLiveData<Movie> movieDetails = new MutableLiveData<>();
+        Api.ApiInterface api = Api.getApi();
+        String movieId = String.valueOf(movie.getId());
+        String apiKey = Api.getApiKey();
+
+        LiveData<Movie> queryresult = getMovieById(movie.getId());
+        queryresult.observeForever(new Observer<Movie>() {
+            @Override
+            public void onChanged(Movie result) {
+                queryresult.removeObserver(this);
+                if (result != null) movie.setFavorite(true);
+            }
+        });
+
+
+        sAppExecutors.networkIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    movie.setReviews(api.fetchReviews(movieId, apiKey).execute().body().getReviews());
+                    movie.setVideos(api.fetchVideos(movieId, apiKey).execute().body().getVideos());
+                    movieDetails.postValue(movie);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return movieDetails;
     }
 }
