60263486901d3302a50cee26ea745a42661967a7
Rene Sauerwein <rene@sauerwein.cc>
2020-04-20 17:12:07 +0200

    Add action bar to detail activity

--- app/src/main/java/cc/sauerwein/popularmovies/DetailActivity.java
+++ app/src/main/java/cc/sauerwein/popularmovies/DetailActivity.java
@@ -35,6 +35,8 @@ public class DetailActivity extends AppCompatActivity {
         mActivityBinding.setLifecycleOwner(this);
         mActivityBinding.setViewModel(mViewModel);
 
+        setSupportActionBar(mActivityBinding.detailActivityToolbar);
+
 
         Intent intent = getIntent();
         String json = intent.getStringExtra(Intent.EXTRA_TEXT);
