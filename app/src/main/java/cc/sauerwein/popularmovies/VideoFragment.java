f6a3b3ade74cdb01b657e51e798c82082697d556
Rene Sauerwein <rene@sauerwein.cc>
2020-04-22 18:59:05 +0200

    Add new video fragment

---
+++ app/src/main/java/cc/sauerwein/popularmovies/VideoFragment.java
@@ -0,0 +1,82 @@
+package cc.sauerwein.popularmovies;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import cc.sauerwein.popularmovies.viewmodels.MovieDetailViewModel;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link VideoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class VideoFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private MovieDetailViewModel mViewModel;
+
+    public VideoFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment VideoFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static VideoFragment newInstance(String param1, String param2) {
+        VideoFragment fragment = new VideoFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+
+//            mViewModel = new ViewModelProvider(requireActivity()).get(MovieDetailViewModel.class);
+//            Log.d("..","halloooo");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mViewModel = new ViewModelProvider(requireActivity()).get(MovieDetailViewModel.class);
+        Log.d("..", "halloooo");
+        return inflater.inflate(R.layout.fragment_video, container, false);
+
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d("..", "halloooo");
+        super.onSaveInstanceState(outState);
+    }
+}
