ba823525161cada53ef31049728baac0cb413560
Rene Sauerwein <rene@sauerwein.cc>
2020-04-24 20:48:38 +0200

    Code cleanup

--- app/src/main/java/cc/sauerwein/popularmovies/VideoFragment.java
+++ app/src/main/java/cc/sauerwein/popularmovies/VideoFragment.java
@@ -1,12 +1,10 @@
 package cc.sauerwein.popularmovies;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -20,34 +18,15 @@ import cc.sauerwein.popularmovies.viewmodels.MovieDetailViewModel;
  * create an instance of this fragment.
  */
 public class VideoFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private MovieDetailViewModel mViewModel;
 
     public VideoFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VideoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VideoFragment newInstance(String param1, String param2) {
+    public static VideoFragment newInstance() {
         VideoFragment fragment = new VideoFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
 
         return fragment;
@@ -56,10 +35,6 @@ public class VideoFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -70,11 +45,5 @@ public class VideoFragment extends Fragment {
         FragmentVideoBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_video, container, false);
         mViewModel.setupVideoRecyclerView(binding.rvVideos);
         return binding.getRoot();
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        Log.d("..", "halloooo");
-        super.onSaveInstanceState(outState);
     }
 }
