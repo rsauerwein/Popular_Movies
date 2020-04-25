d9033b94870c0f47fbe63c3a95d65fa4388049ed
Rene Sauerwein <rene@sauerwein.cc>
2020-04-25 11:39:48 +0200

    fix doc comment

--- app/src/main/java/cc/sauerwein/popularmovies/viewmodels/MovieDetailViewModel.java
+++ app/src/main/java/cc/sauerwein/popularmovies/viewmodels/MovieDetailViewModel.java
@@ -58,7 +58,7 @@ public class MovieDetailViewModel extends AndroidViewModel {
     }
 
     /**
-     *
+     * Fetch trailers and reviews
      */
     private void fetchMovieDetails() {
         MutableLiveData<Movie> details = mRepository.getMovieDetails(mMovie);
